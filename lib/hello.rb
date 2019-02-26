3813a381f2d34958c4ec02baa7aab750498fa748
Dennis Chung <chungdennis6@gmail.com>
2019-02-26 01:26:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,10 @@
 def hello_t
-if block_given?
+ if block_given?
     i = 0
 
-
      while i < array.length
-      yield(array[i])
-      i += 1
+      yield array [i]
+      i = i + 1
     end
 
      array
